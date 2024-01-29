1b17f893d17868ddc0d320a6b372bec2c9c1b2b0
JeremiahID <jeremiahefe.je@gmail.com>
2024-01-29 11:35:07 +0100

    Leap Year calculator

--- index.js
+++ index.js
@@ -15,3 +15,24 @@ function fizzBuzz(){
         number.push(count)
     }
 }
+
+// This is a program to calculate for leap year
+
+const leapYear = prompt("Enter the year");
+
+function calYear(){
+    if (leapYear % 4 === 0){
+        alert("This Year is a leap year")
+
+    } else if (leapYear % 4 === 0 && leapYear % 100 === 0) {
+        alert("this is not a leap year")
+
+    } else if (leapYear % 4 === 0 && leapYear % 100 === 0 && leapYear % 4000 === 0){
+        alert("This Year is a leap year")
+    } else {
+        alert("This is not a leap Year")
+    }
+
+}
+
+calYear()
