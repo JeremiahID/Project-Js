2d2c9f55c99e0119ece145e78d2bf22c44cd6420
JeremiahID <jeremiahefe.je@gmail.com>
2024-01-29 12:14:06 +0100

    fizz buzz updates

--- index.js
+++ index.js
@@ -4,35 +4,39 @@ var number = [];
 count = 1;
 
 function fizzBuzz(){
+    while(count <= 100){
+        if(count % 3 === 0 && count % 5 === 0){
+            number.push("fizzBuzz")
+        } else if(count % 3 === 0){
+            number.push("fizz")
+        } else if (count % 5 === 0){
+            number.push("Buzz")
+        } else{
+            number.push(count)
+        }
+        count++;
 
-    if(count % 3 === 0 && count % 5 === 0){
-        number.push("fizzBuzz")
-    } else if(count % 3 === 0){
-        number.push("fizz")
-    } else if (count % 3 === 0){
-        number.push("Buzz")
-    } else{
-        number.push(count)
     }
+    console.log(number)
 }
 
 // This is a program to calculate for leap year
 
-const leapYear = prompt("Enter the year");
+// const leapYear = prompt("Enter the year");
 
-function calYear(){
-    if (leapYear % 4 === 0){
-        alert("This Year is a leap year")
+// function calYear(){
+//     if (leapYear % 4 === 0){
+//         alert("This Year is a leap year")
 
-    } else if (leapYear % 4 === 0 && leapYear % 100 === 0) {
-        alert("this is not a leap year")
+//     } else if (leapYear % 4 === 0 && leapYear % 100 === 0) {
+//         alert("this is not a leap year")
 
-    } else if (leapYear % 4 === 0 && leapYear % 100 === 0 && leapYear % 4000 === 0){
-        alert("This Year is a leap year")
-    } else {
-        alert("This is not a leap Year")
-    }
+//     } else if (leapYear % 4 === 0 && leapYear % 100 === 0 && leapYear % 4000 === 0){
+//         alert("This Year is a leap year")
+//     } else {
+//         alert("This is not a leap Year")
+//     }
 
-}
+// }
 
-calYear()
+// calYear()
