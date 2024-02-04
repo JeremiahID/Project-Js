235ed96cc58040cc761ef69f308a826608670228
JeremiahID <jeremiahefe.je@gmail.com>
2024-02-04 15:51:17 +0100

    test

--- index.js
+++ index.js
@@ -3,43 +3,43 @@
 var number = [];
 count = 1;
 
-// function fizzBuzz(){
-//     while(count <= 100){
-//         if(count % 3 === 0 && count % 5 === 0){
-//             number.push("fizzBuzz")
-//         } else if(count % 3 === 0){
-//             number.push("fizz")
-//         } else if (count % 5 === 0){
-//             number.push("Buzz")
-//         } else{
-//             number.push(count)
-//         }
-//         count++;
+ function fizzBuzz(){
+     while(count <= 100){
+         if(count % 3 === 0 && count % 5 === 0){
+             number.push("fizzBuzz")
+         } else if(count % 3 === 0){
+             number.push("fizz")
+         } else if (count % 5 === 0){
+            number.push("Buzz")
+         } else{
+             number.push(count)
+         }
+         count++;
 
     }
     console.log(number)
 }
 
-//This is a program to calculate for leap year
+This is a program to calculate for leap year
 
-// const leapYear = prompt("Enter the year");
+const leapYear = prompt("Enter the year");
 
-// function calYear(){
-//     if (leapYear % 4 === 0){
-//         alert("This Year is a leap year")
+function calYear(){
+    if (leapYear % 4 === 0){
+        alert("This Year is a leap year")
 
-//     } else if (leapYear % 4 === 0 && leapYear % 100 === 0) {
-//         alert("this is not a leap year")
+    } else if (leapYear % 4 === 0 && leapYear % 100 === 0) {
+        alert("this is not a leap year")
 
-//     } else if (leapYear % 4 === 0 && leapYear % 100 === 0 && leapYear % 4000 === 0){
-//         alert("This Year is a leap year")
-//     } else {
-//         alert("This is not a leap Year")
-//     }
+    } else if (leapYear % 4 === 0 && leapYear % 100 === 0 && leapYear % 4000 === 0){
+        alert("This Year is a leap year")
+    } else {
+        alert("This is not a leap Year")
+    }
 
-// }
+}
 
-// calYear()
+calYear()
 
 
 // This is a program for the fibonacci logic
@@ -57,9 +57,12 @@ function fibonacciGenerator (n){
         output = [0,1]
 
     } else {
-        alert("This is not a leap Year")
+        output = [0,1]
+
+        for(counter =2; counter < n; counter++){
+
+        }
     }
 
 }
 
-calYear()
